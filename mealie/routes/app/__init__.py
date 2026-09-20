2fa076cf2a6716010e24ca92fe10ab0ddbf3e672
smugenai <lwynter@smu.edu.sg>
2026-09-20 16:42:02 +0800

    feat(api): expose supported locales endpoint

--- mealie/routes/app/__init__.py
+++ mealie/routes/app/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from . import app_about
+from . import app_about, app_locales
 
 router = APIRouter(prefix="/app")
 
 router.include_router(app_about.router, tags=["App: About"])
+router.include_router(app_locales.router, tags=["App: Locales"])
